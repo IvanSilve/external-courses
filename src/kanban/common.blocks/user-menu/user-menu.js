ac2c23dd4083a808c5fba7607c60a448b8778f0a
IvanSilve <gitIvanSilve@mail.ru>
2020-04-26 16:20:56 +0300

    ex13 js events implementation of adding a new task

--- src/kanban/common.blocks/user-menu/user-menu.js
+++ src/kanban/common.blocks/user-menu/user-menu.js
@@ -5,37 +5,41 @@ function openMenu() {
     userMenuBtn.classList.remove('user-menu-btn--closed');
     userMenuBtn.classList.add('user-menu-btn--opened');
 
-    let main = document.getElementsByClassName('main')[0];
-    let userMenu = document.createElement('nav');
+    let header = document.getElementsByClassName('header')[0];
+    let userMenu = document.createElement('ul');
     userMenu.classList.add('user-menu');
-    let myAccount = document.createElement('a');
+    let myAccount = document.createElement('li');
     myAccount.classList.add('user-menu__item');
     myAccount.setAttribute('href', '#');
     myAccount.innerHTML = "My account";
-    let myTasks = document.createElement('a');
+    let myTasks = document.createElement('li');
     myTasks.classList.add('user-menu__item');
     myTasks.setAttribute('href', '#');
     myTasks.innerHTML = "My tasks";
-    let logOut = document.createElement('a');
+    let logOut = document.createElement('li');
     logOut.classList.add('user-menu__item');
     logOut.setAttribute('href', '#');
     logOut.innerHTML = "Log out";
 
-    main.prepend(userMenu);
+    header.append(userMenu);
     userMenu.append(myAccount);
     userMenu.append(myTasks);
     userMenu.append(logOut);
-    userMenuBtn.addEventListener('click', closeMenu, {once: true});
+    userMenuBtn.addEventListener('click', closeMenu);
+    userMenuBtn.addEventListener('blur', closeMenu);
 }
 function closeMenu() {
+
     let userMenuBtn = document.getElementsByClassName('user-menu-btn')[0];
     userMenuBtn.classList.remove('user-menu-btn--opened');
     userMenuBtn.classList.add('user-menu-btn--closed');
+    userMenuBtn.removeEventListener('click', closeMenu);
+    userMenuBtn.removeEventListener('blur', closeMenu);
 
     let userMenu = document.getElementsByClassName('user-menu')[0];
     userMenu.classList.add('user-menu--close');
     setTimeout(function () {
         userMenu.remove();
         userMenuBtn.addEventListener('click', openMenu, {once: true});
-    }, 600);
+    }, 560);
 }
